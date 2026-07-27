33e644d5d9757ed6a143fb8b01f77f8141964289
Michael Musatov <mv.musatov@gmail.com>
2026-07-27 18:51:38 +0400

    ENG-655 Propagate copy link event from uikit to the app

--- src/v4/core/providers/PageBehaviorProvider.tsx
+++ src/v4/core/providers/PageBehaviorProvider.tsx
@@ -258,6 +258,7 @@ export interface PageBehavior {
     handleNonFollowerAction?(context: { alignment: NotificationAlignment }): void;
     onPostProductTagClick?(context: { product: Amity.Product }): void;
     onLivestreamProductTagClick?(context: { product: Amity.Product }): void;
+    onPostLinkCopied?(context: { postId: string; link: string }): void;
   };
   AmityEventTargetSelectionPageBehavior?: {
     goToEventSetupPage?(context: EventSetupProps): void;
@@ -1010,6 +1011,11 @@ export const PageBehaviorProvider: React.FC<PageBehaviorProviderProps> = ({
         }
         context.product?.productUrl &&
           window.open(context.product.productUrl, '_blank', 'noopener,noreferrer');
+      },
+      onPostLinkCopied: (context: { postId: string; link: string }) => {
+        if (pageBehavior?.AmityGlobalBehavior?.onPostLinkCopied) {
+          return pageBehavior.AmityGlobalBehavior.onPostLinkCopied(context);
+        }
       },
     },
     AmityMyCommunitiesComponentBehavior: {
