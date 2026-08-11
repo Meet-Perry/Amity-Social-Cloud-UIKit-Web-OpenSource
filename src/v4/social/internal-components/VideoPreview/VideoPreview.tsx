d59393ecfc5ef246a24ec1c79701f17fa4d6a2ab
Michael Musatov <mv.musatov@gmail.com>
2026-08-11 18:39:03 +0400

    ENG-899 Fixed problem with video not playing in chrome

--- src/v4/social/internal-components/VideoPreview/VideoPreview.tsx
+++ src/v4/social/internal-components/VideoPreview/VideoPreview.tsx
@@ -8,7 +8,14 @@ type VideoPreviewProps = {
 
 export const VideoPreview = React.forwardRef<HTMLVideoElement, VideoPreviewProps>(
   ({ src, mimeType, mediaFit, ...props }, ref) => (
-    <video controls controlsList="nodownload" {...props} ref={ref} data-testid="video-preview">
+    <video
+      controls
+      controlsList="nodownload"
+      crossOrigin="anonymous"
+      {...props}
+      ref={ref}
+      data-testid="video-preview"
+    >
       <source src={src} type={mimeType} />
       <p>
         Your browser does not support this format of video. Please try again later once the server
